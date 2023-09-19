a52142a28ab899c2fa7dbb563729df08c40e772b
Haseeb Khan <haseeb999khan@gmail.com>
2023-09-19 20:25:45 +0500

    feat: :sparkles: Left side bar complete UI done

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4">
+      <div className="px-6">
+        <LeftSidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
