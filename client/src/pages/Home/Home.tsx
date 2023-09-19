eb52e3550ac17f7534ffd3e2b051badf3fe652cd
Haseeb Khan <haseeb999khan@gmail.com>
2023-09-19 20:40:20 +0500

    feat: :sparkles: Right side bar complete UI done

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 
 type Props = {};
 
@@ -8,6 +10,12 @@ const Home = (props: Props) => {
     <div className="grid grid-cols-1 md:grid-cols-4">
       <div className="px-6">
         <LeftSidebar />
+      </div>
+      <div className="col-span-2 border-x-2 border-t-slate-800 px-6">
+        Main tweet goes here
+      </div>
+      <div className="px-6">
+        <RightSidebar />
       </div>
     </div>
   );
