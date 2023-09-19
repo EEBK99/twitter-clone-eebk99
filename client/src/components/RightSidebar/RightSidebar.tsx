b7f380a45c98eb0c80b1ca645751155290e63d2b
Haseeb Khan <haseeb999khan@gmail.com>
2023-09-19 20:39:49 +0500

    feat: :sparkles: Right side bar complete UI done

---
+++ client/src/components/RightSidebar/RightSidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+const RightSidebar = (props: Props) => {
+  return (
+    <div className="p-6 bg-slate-100 rounded-lg mx-4 space-y-4">
+      <h2 className="font-medium">Trending</h2>
+      <p className="font-bold">#gryajsbdsad</p>
+      <p className="font-bold">#asdkjjsausd</p>
+      <p className="font-bold">#hjgdsuwteqw</p>
+      <p className="font-bold">#iotirnksand</p>
+    </div>
+  );
+};
+
+export default RightSidebar;
