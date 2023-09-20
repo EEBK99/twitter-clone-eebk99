f90e0a09f61974041a7f70200bd88874ba709272
Haseeb Khan <haseeb999khan@gmail.com>
2023-09-20 17:51:04 +0500

    feat: :sparkles: Main Tweet API integration done

--- client/src/components/MainTweet/MainTweet.tsx
+++ client/src/components/MainTweet/MainTweet.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
 import TimelineTweets from "../TimelineTweets/TimelineTweets";
 
 type Props = {};
 
 const MainTweet = (props: Props) => {
+  const { currentUser } = useSelector((state: any) => state.user);
+  const [tweetText, setTweetText] = useState("");
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const submitTweet = await axios.post("/tweets", {
+        userId: currentUser._id,
+        description: tweetText,
+      });
+      window.location.reload();
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <div>
-      <p className="font-bold pl-2 my-2">UserName</p>
+      {currentUser && (
+        <p className="font-bold pl-2 my-2">{currentUser.username}</p>
+      )}
       <form className="border-b-2 pb-6">
         <textarea
+          onChange={(e) => setTweetText(e.target.value)}
           placeholder="What's happening"
           maxLength={280}
           className="bg-slate-200 rounded-lg w-full p-2"
-        ></textarea>
+        />
 
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-full ml-auto">
+        <button
+          onClick={handleSubmit}
+          className="bg-blue-500 text-white py-2 px-4 rounded-full ml-auto"
+        >
           Tweet
         </button>
       </form>
